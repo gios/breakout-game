f2ecd2a8fbdba49fc312d6e7fc8bf1d3a62bb254
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2016-07-18 21:47:33 +0300

    rimraf

--- index.ts
+++ index.ts
@@ -5,6 +5,6 @@ let canvas = <HTMLCanvasElement> document.getElementById("game");
 let ctx: CanvasRenderingContext2D = canvas.getContext("2d");
 
 let canvasContext = new CanvasContext(canvas, ctx);
-let ball = new Ball(canvasContext, 10, canvasContext.getCanvasWidth() / 4, canvasContext.getCanvasHeight() - 30, 2, -2);
+let ball = new Ball(canvasContext, 10, canvasContext.getCanvasWidth() / 2, canvasContext.getCanvasHeight() - 30, 2, -2);
 canvasContext.setBall(ball);
 canvasContext.draw();
