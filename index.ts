d1de57a4a2638b7848ad15d4a1a2eef40c95a1fa
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2016-07-19 15:05:37 +0300

    canvas size

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import {CanvasContext} from "./src/CanvasContext";
 import {Paddle} from "./src/Paddle";
 
 let ball = new Ball(10, CanvasContext.getCanvasWidth() / 2, CanvasContext.getCanvasHeight() - 30, 2, -2);
-let paddle = new Paddle(10, 75, (CanvasContext.getCanvasWidth() - 75) / 2);
+let paddle = new Paddle(10, 140, (CanvasContext.getCanvasWidth() - 140) / 2);
 
 CanvasContext.setItem(ball);
 CanvasContext.setItem(paddle);
